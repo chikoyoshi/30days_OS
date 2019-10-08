2341a25f550fd1bbd5ee15b4f445cd387bbd86c7
Yoshiki from linux <chikoyoshi-21141@outlook.jp>
2019-10-08 19:34:37 +0900

    3日目終了

---
+++ 03day/bootpack.c
@@ -0,0 +1,9 @@
+void io_hlt(void);
+
+void HariMain(void)
+{
+    fin:
+    /*  HLT命令はC言語にない関数を作成    */
+    /*io_hlt();*/
+    goto fin;
+}
---
+++ 03day/nasmfunc.asm
@@ -0,0 +1,9 @@
+; nasmfunc
+
+
+section .text         ;オブジェクトファイルではこれを書いてからプログラムを書く
+    GLOBAL  io_hlt
+
+io_hlt:                ;void io_hlt(void);
+    HLT
+    RET
